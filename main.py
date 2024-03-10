9e34dd21c58311ba10902e2358a96282b3909691
Neeraj Sahu <neerajsahu1022@gmail.com>
2024-03-10 19:17:40 +0530

    Data Ingestion completed

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from src.LD_Classifier.logger import logger
 from src.LD_Classifier.Exception import CustomException
+from src.LD_Classifier.pipeline.Stage_01_data_ingestion import  DataIngestionTrainingPipeline
 import os,sys
 
 
+STAGE_NAME = "Data Ingestion stage"
+
+
 try:
-    logger.info(f"First Log welcome 09-03-2024")
-    a=1/0
-    
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         error = CustomException(e, sys)
         logger.info(error.error_message)
+
+    
---
+++ src/LD_Classifier/components/Data_Ingestion.py
@@ -0,0 +1,46 @@
+import os,sys
+import zipfile
+import gdown
+from LD_Classifier.logger import logger
+from LD_Classifier.Exception import CustomException
+from LD_Classifier.utils.common import get_size
+from LD_Classifier.entity.config_entity import DataIngestionConfig
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+
+     
+    def download_file(self)-> str:
+        '''
+        Fetch data from the url
+        '''
+
+        try: 
+            dataset_url = self.config.source_URL
+            zip_download_dir = self.config.local_data_file
+            os.makedirs("artifacts/data_ingestion", exist_ok=True)
+            logger.info(f"Downloading data from {dataset_url} into file {zip_download_dir}")
+
+            file_id = dataset_url.split("/")[-2]
+            prefix = 'https://drive.google.com/uc?/export=download&id='
+            gdown.download(prefix+file_id,zip_download_dir)
+
+            logger.info(f"Downloaded data from {dataset_url} into file {zip_download_dir}")
+
+        except Exception as e:
+            error = CustomException(e, sys)
+            logger.info(error.error_message)
+        
+    
+    def extract_zip_file(self):
+        """
+        zip_file_path: str
+        Extracts the zip file into the data directory
+        Function returns None
+        """
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
