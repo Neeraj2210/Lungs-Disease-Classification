3405c14da6d03ebebba6dbabdeb49b8cf622765c
Neeraj Sahu <neerajsahu1022@gmail.com>
2024-03-13 10:22:12 +0530

    Model Trainig Completed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.LD_Classifier.logger import logger
 from src.LD_Classifier.Exception import CustomException
 from src.LD_Classifier.pipeline.Stage_01_data_ingestion import  DataIngestionTrainingPipeline
 from src.LD_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from src.LD_Classifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -26,8 +26,25 @@ try:
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
     error = CustomException(e, sys)
     logger.info(error.error_message)
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+    error = CustomException(e, sys)
+    logger.info(error.error_message)
+
+
+
 
     
